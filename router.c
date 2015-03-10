0ae27ea0b43b530342eb73090f59015233060180
Neel Parikh <nkp.parikh@gmail.com>
2015-03-09 19:58:45 -0600

    Able to read through the packet forwarding table, and determine where to forward the packets.

--- router.c
+++ router.c
@@ -9,9 +9,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <ctype.h>
 #define MAXBUFSIZE 4096
-#define IPSIZE 16
+#define LINESIZE 80
 
 /* for storing received packet information */
 struct packetInfo
@@ -26,15 +26,22 @@ struct packetInfo
 /* for purposes of statistics */
 struct statistics
 {
-	unsigned long directDelPkt_count;			/* running count of packets directly delivered */
-	unsigned long forwardedPkt_count;			/* running count of forwarded packets */
-	unsigned long unroutedPkt_count;			/* running count of dropped packets */
-	unsigned long expiredPkt_count;				/* running count of expired packets */
+	unsigned long directDelPkt_count;			/* count of packets directly delivered (to network A)*/
+	unsigned long BPkt_count;					/* count of forwarded packets to network B */
+	unsigned long CPkt_count;					/* count of forwarded packets to network C */ 
+	unsigned long unroutedPkt_count;			/* count of dropped packets */
+	unsigned long expiredPkt_count;				/* count of expired packets */
 } stats = {};
 
+enum Route{ A, B, C, FAIL};
 
 void usage();
-int getPktInfo(char buf[]);
+int getPktInfo(char buf[]);								/* retreive the info from packets store into pktInfo struct*/
+enum Route findRoutingPath(struct sockaddr_in);		/* to determine which network to forward the packet*/
+
+FILE *fp_route;	/* global file pointer to reference the routing table file path */
+FILE *fp_stats;	/* global file pointer to reference the statistic file path */
+
 /*  Program takes three command-line arguments: */
 /*  <port number to listen> <routing table file path> <statistic file path> */
 /* the router program */
@@ -46,6 +53,7 @@ int main(int argc, char * argv[])
 	int recv_len;	/* to store return value from recvfrom()*/
 	int routerSocket;	/* to store socket descriptor*/
 	char buf[MAXBUFSIZE];
+	enum Route pktRoute;
 
 	/* check for correct command line argument */
 	if(argc != 4) usage();
@@ -54,7 +62,20 @@ int main(int argc, char * argv[])
 	const char * routingTable_Path = argv[2]; 		/* store routing table file path */
 	const char * statistic_Path = argv[3]; 			/* store statistic file path */
 
-	
+	/* report error if routing table file cannot be opened */
+	if((fp_route = fopen(routingTable_Path,"r")) == NULL)
+	{
+		perror("routing table path cannot be opened.");
+		exit(1);
+	}
+	/* report error if statistic file cannot be opened */
+	if((fp_stats = fopen(statistic_Path,"w")) == NULL)
+	{
+		perror("statistic file cannot be opened.");
+		exit(1);
+	}
+
+
 	/* create socket from which to read */
 	routerSocket = socket(AF_INET, SOCK_DGRAM, 0);
 	if(routerSocket < 0)
@@ -83,13 +104,16 @@ int main(int argc, char * argv[])
 							(struct sockaddr *) &remoteaddr, &remote_addrlen);
 		if(recv_len > 0)
 		{
-			printf("received message: %s\n", buf);
+			printf("\nreceived message: %s\n", buf);
 			unsigned int readingStatus = getPktInfo(buf);
+			/* The packet was processed and the TTL became zero */
 			if(readingStatus == 0)
 			{
 				/* TLL is zero */
 				printf("\texpired packet.\n");
+				stats.expiredPkt_count++;	/* increment expired packets count */
 			}
+			/* The packet was processed and it was a valid packet */
 			else if(readingStatus == 1)
 			{
 	
@@ -101,13 +125,41 @@ int main(int argc, char * argv[])
 				char destinationIP_str[INET_ADDRSTRLEN];
 				inet_ntop(AF_INET, &(pktInfo.destination.sin_addr), destinationIP_str, INET_ADDRSTRLEN);
 				printf("\tDestionation IP: %s\n",destinationIP_str);
-				printf("\tTTL: %hu\n", pktInfo.timetolive);
+				printf("\tTTL: %hu\n\n", pktInfo.timetolive);
+				/* Determine where this packet should be forwarded */
+				pktRoute = findRoutingPath(pktInfo.destination);
+				switch(pktRoute)
+				{
+					/* forward packet to network A*/
+					case A:
+						printf("\tThis packet will be directly delivered.\n");
+						stats.directDelPkt_count++;
+						break;
+					/* forward packet to network B*/
+					case B:
+						printf("\tThis packet will be forwarded to routerB.\n");
+						stats.BPkt_count++;
+						break;
+					/* forward packet to network C*/
+					case C:
+						printf("\tThis packet will be forwarded to routerC.\n");
+						stats.CPkt_count++;
+						break;
+					/* packet cannot be routed */
+					case FAIL:
+						printf("\tThis packet cannot be routed.\n");
+						stats.unroutedPkt_count++;
+						break;
+				}
 			}
+			/* The packet was processed and it did not have a valid destination address*/
 			else
 			{
-				printf("\tinvalid destionation address packet.\n");
+				printf("\tinvalid destination address packet.\n");
+				stats.unroutedPkt_count++;	/* increment unrouted packet count */
 			}
 		}
+		/* could not receive packet */
 		else if(recv_len < 0)
 		{
 			perror("error is receiving packet.");
@@ -116,6 +168,54 @@ int main(int argc, char * argv[])
 	} 
 	/* never exits */
 	return 0;
+}
+
+enum Route findRoutingPath(struct sockaddr_in packetDestination)
+{
+	char line[LINESIZE];	/* store each line read in file */
+
+	/*for each line read*/
+	char NetIP[INET_ADDRSTRLEN];	/* store the network address */
+	unsigned short prefix;			/* store the network prefix*/
+	char router[8];					/* store the hop value*/
+
+
+	char DestIPMasked[INET_ADDRSTRLEN];		/* store the destination IP string in dot notation after masked*/
+
+	/*also need certain values to store the the masked IP in network byte order*/
+	struct in_addr destinAddr = packetDestination.sin_addr;
+	unsigned long destinIP = ntohl(destinAddr.s_addr);
+	unsigned long destinIP_masked;
+
+	enum Route takeRoute;
+
+	rewind(fp_route); /*reset the file pointer to the beginning of the file */
+	while(fgets(line,LINESIZE,fp_route))
+	{
+		size_t ln = strlen(line)-1;
+		if((line[0] != '\n') && (line[0] != '\r'))
+		{
+			line[ln] = '\0';
+			sscanf(line,"%s %hu %s",NetIP,&prefix,router);
+
+			if(strcmp(router,"RouterB") == 0) takeRoute = B;
+			else if(strcmp(router,"RouterC") == 0) takeRoute = C;
+			else takeRoute = A;
+
+			/*extract <prefix> number of bits of the destination IP*/
+			destinIP_masked = destinIP & (0xFFFFFFFF << (32-prefix));
+			/*convert the masked destinIP to network order byte*/
+			destinAddr.s_addr = htonl(destinIP_masked);
+			/* get the string dot notation representation of this IP */
+			inet_ntop(AF_INET, &(destinAddr), DestIPMasked, INET_ADDRSTRLEN);
+
+			if(strcmp(NetIP,DestIPMasked) == 0)
+				return takeRoute;
+
+		}
+	}
+	
+	return FAIL;
 }
 /* update packetInfo struct with the received data*/
 int getPktInfo(char buf[])
